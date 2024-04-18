dfe38ab1b5ff52810ff4fabd510f51a0c84a8854
Elif <eliiftum@gmail.com>
2024-04-18 12:05:17 +0300

    config files created

---
+++ MobvenProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobvenProject
+//
+//  Created by Elif Tum on 17.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        let mainURL = Bundle.main.infoDictionary?["BACKEND_URL"] as? String
+//        print(mainURL)
+    }
+
+
+}
+
